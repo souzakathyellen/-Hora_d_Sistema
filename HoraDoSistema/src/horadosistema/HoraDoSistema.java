cfdd0e016cd29a4d499c719053b87dccf1e0ac06
Kathyellen Souza <87395970+souzakathyellen@users.noreply.github.com>
2022-10-25 16:43:13 -0300

    Add files via upload

---
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,13 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+    
+    public static void main(String[] args) {
+        Date data = new Date();
+
+        System.out.println("A hora é :" + data.toString());
+    }
+
+}
